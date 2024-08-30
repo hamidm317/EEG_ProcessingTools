8566a1280777e0290de88d1522e6a75e4485bcd8
hamidm317 <hamidm317@gmail.com>
2024-08-31 02:08:33 +0330

    Add Functions for F-Era

--- Utils/Local.py
+++ Utils/Local.py
@@ -1,8 +1,13 @@
 import h5py
 from pandas import read_excel, read_csv
 import numpy as np
+import os
+import pandas as pd
+from datetime import datetime
 
-from Utils.Constants import LocalDataConstants
+import pickle
+
+from Utils.Constants import LocalDataConstants, DC_Constants
 
 def ClusteredEEGLoader(event):
 
@@ -82,3 +87,72 @@ def AvailableSubjects():
         available_IDs.append(BehavioralData['id'][idx])
 
     return np.array(subjects_of_interest), available_IDs
+
+def HandleDir(Directory):
+
+    if not os.path.isdir(Directory):
+
+        os.makedirs(Directory)
+
+    return Directory
+
+def HandleFileName(SaveFileDir, specs):
+
+    CurrentTime = datetime.now()
+
+    if not os.path.isfile(SaveFileDir + "\\VersionHistory.csv"):
+
+        HistoryDict = {'idx': 0, 'VersionNumber': [0], 'Date': [str(CurrentTime.year) + '-' + str(CurrentTime.month) + '-' + str(CurrentTime.day)], 
+                       'Window_Length': [specs['window_length']], 'Overlap_Ratio': [specs['overlap_ratio']], 'Start Time': [specs['start time']],
+                       'End Time': [specs['end time']],
+                       'OrdersMatrix': [specs['orders_matrix']]}
+        DF = pd.DataFrame(HistoryDict)
+
+        DF.to_csv(SaveFileDir + "\\VersionHistory.csv", index = False)
+
+        version_number = 0
+
+    else:
+
+        HistoryDict = read_csv(SaveFileDir + "\\VersionHistory.csv", index_col = 0)
+
+        version_number = HistoryDict['VersionNumber'][len(HistoryDict['VersionNumber']) - 1] + 1
+
+        new_row = [version_number, str(CurrentTime.year) + '-' + str(CurrentTime.month) + '-' + str(CurrentTime.day), specs['window_length'],
+                   specs['overlap_ratio'], specs['start time'], specs['end time'], specs['orders_matrix']]
+        
+        HistoryDict.loc[len(HistoryDict['VersionNumber'])] = new_row
+        HistoryDict.sort_index()
+
+        HistoryDict.to_csv(SaveFileDir + "\\VersionHistory.csv")
+
+    SaveFileName = "Data_Version" + str(version_number)
+
+    return SaveFileName, version_number
+
+def BandAvailable(Kernel, Band):
+
+    if Band in DC_Constants.Properties[Kernel]['AvailableBands']:
+
+        return True
+    
+    else:
+
+        return False
+    
+def HandleDataLoad(Dir, version_number = None):
+
+    assert os.path.isdir(Dir), "This Data is not Available"
+
+    if version_number is None:
+
+        VersionHistoryDF = read_csv(Dir + "\\VersionHistory.csv", index_col = 0)
+        version_number = np.array(VersionHistoryDF['VersionNumber'])[0]
+
+    LoadFileDir = Dir + "\\Data_Version" + str(version_number)
+
+    with open(LoadFileDir, 'rb') as f:
+
+        ConDataDict = pickle.load(f)
+
+    return ConDataDict
