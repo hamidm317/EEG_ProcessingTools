72f9251d0e204035f5e4caa928bdbded5bac025b
hamidm317 <hamidm317@gmail.com>
2024-08-31 02:09:14 +0330

    Add AestDim to find most Aesthetical Dimensions in Figures

--- Utils/SciPlot.py
+++ Utils/SciPlot.py
@@ -105,3 +105,23 @@ def ConfidenceBound(Data, Confidence_Level):
     LB_CI = np.mean(Data) - Confidence_Level * np.std(Data)
 
     return UB_CI, LB_CI
+
+def AestDim(Q, thr = 3):
+
+    CanDims_t = []
+
+    for i in range(1, int(Q / 2) + 1):
+
+        if np.mod(Q, i) == 0:
+
+            CanDims_t.append([i, int(Q / i)])
+
+    BestDims = CanDims_t[-1]
+
+    if max([BestDims[0] / BestDims[1], BestDims[1] / BestDims[0]]) <= thr:
+
+        return np.sort(CanDims_t[-1])
+    
+    else:
+        
+        return AestDim(Q + 1)
